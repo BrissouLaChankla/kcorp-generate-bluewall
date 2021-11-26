236eeb277f7024f6d1209560e8f30fb1214cd7fc
Brice Eliasse <brice.eliasse@gmail.com>
2021-11-26 01:26:20 +0100

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>KC Blue Wall - Generate your twitter profile's picture</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" ></script>
+    
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+    <link rel="icon" type="{{asset('img/logo.png')}}" href="logo.png" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Finger+Paint&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="vw-100 vh-100 bg-secondary" >
+    @yield('content')
+    @yield('scripts')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,370 @@
+@extends("layouts.app")
+@section('content')
+<div class="w-100 h-100 container d-flex flex-column justify-content-around" id="app">
+    <div class="text-center" >
+        <img style="width:120px" class="border-primary border-bottom" src="{{asset('img/supporter.png')}}" alt="Supporter KCorp">
+        <h1 class="text-uppercase text-white"><strong>KCorp <br> Blue wall</strong></h1>
+    </div>
+    <div class="row justify-content-center align-items-center" >
+        <div class="col-sm-6">
+            {!! Form::open(['url' => 'https://sdk.photoroom.com/v1/segment', "id" =>"kcorp",'class' => ' ajax-and-picture', 'files' => true]) !!}
+                <div class="drop-zone">
+                    <span class="drop-zone__prompt">Drag and drop ta photo de bg ici</span>
+                    {{Form::file('image_file', ['id' => "file-input", "class" => "drop-zone__input"])}}
+                </div>
+            {!! Form::close() !!}
+        </div>
+
+        <div class="col-sm-6 mt-3 mt-sm-0">
+          <div class="background-kc rounded shadow-sm position-relative" style="background-image:url({{asset('img/wall.jpg')}})">
+            <span class="text-white frais">Comment t'es trop frais... 🥵</span>
+                <div id="ur-pic"></div>
+            </div>
+        </div>
+    </div> 
+ 
+        <div class="text-center">
+            <div class="btn btn-primary btn-lg my-3"  id="pic-download">
+            Télécharger ta nouvelle PP
+            </div>
+        </div>
+   
+
+</div>
+
+<style>
+
+  .frais {
+    font-family: 'Finger Paint', cursive;
+    position: absolute;
+    width: 100%;
+    transform: translateX(-50%);
+    left: 50%;
+    opacity: .9;
+    bottom: -25px;
+    display: none;
+  }
+h1 {
+
+  font-family: 'Bebas Neue', cursive;
+  letter-spacing: 2px;
+  font-size:4rem;
+  line-height: 1;
+}
+    
+.drop-zone {
+  margin:auto;
+    width:65%;
+    padding: 4%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-family: "Quicksand", sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    cursor: pointer;
+    color: #cccccc;
+    border: 4px dashed #00d8ec;
+    border-radius: 10px;
+}
+
+@media only screen and (min-width: 576px) {
+  .drop-zone {
+    height: 250px!important;
+    width: 250px!important;
+  }
+
+  h1 {
+    font-size:4.5rem;
+  }
+
+  .background-kc {
+    height: 250px!important;
+    width: 250px!important;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .drop-zone {
+    height: 340px!important;
+    width: 340px!important;
+  }
+  h1 {
+    font-size:5rem;
+  }
+  .background-kc {
+    height: 340px!important;
+    width: 340px!important;
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  .drop-zone {
+    height: 450px!important;
+    width: 450px!important;
+  }
+
+  .background-kc {
+    height: 450px!important;
+    width: 450px!important;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  .drop-zone {
+    height: 500px!important;
+    width: 500px!important;
+  }
+
+  .background-kc {
+    height: 500px!important;
+    width: 500px!important;
+  }
+}
+
+
+.drop-zone--over {
+  border-style: solid;
+}
+
+.drop-zone__input {
+  display: none;
+}
+
+.drop-zone__thumb {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: #cccccc;
+  background-size: cover;
+  position: relative;
+}
+
+.drop-zone__thumb::after {
+  content: attr(data-label);
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 5px 0;
+  color: #ffffff;
+  background: rgba(0, 0, 0, 0.75);
+  font-size: 14px;
+  text-align: center;
+}
+
+    #ur-pic {
+        background-position: center;
+        background-size: cover;
+        height: 100%;
+        width:100%;
+    }
+
+    .background-kc {
+      width:65%;
+      margin:auto;
+        background-size:cover;
+        background-position: center;
+        position: relative;
+    }
+
+    #pic-download {
+      display:none;
+    }
+</style>
+@endsection
+      
+@section("scripts")
+<script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+<script>
+    $(function() {
+    var $window = $(window);
+    var $pane = $('#pane1');
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < 576) {
+          console.log("caca");
+              $('.drop-zone').height($(".drop-zone").width());
+              $('.background-kc').height($(".background-kc").width());
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+
+
+   
+
+        document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+  const dropZoneElement = inputElement.closest(".drop-zone");
+
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+      submitForm();
+     
+
+    }
+  });
+
+  dropZoneElement.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZoneElement.classList.add("drop-zone--over");
+  });
+
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+
+  dropZoneElement.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      submitForm();
+     
+
+    }
+
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
+});
+
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function submitForm() {
+            var myForm = $("#kcorp")[0];
+            var url = $("#kcorp").attr('action');
+            jQuery.ajax({
+                type: "POST",
+                url:url,
+                cache:false,
+                processData: false,
+                contentType: false,
+                headers: { 'x-api-key': '147d07b083abc001c55bbc9ba5c3138c79711358' },
+                data: new FormData(myForm),
+                xhr:function(){
+                    var xhr = new XMLHttpRequest();
+                    xhr.responseType= 'blob'
+                    return xhr;
+                },
+                success: function(data){
+                    var img = document.getElementById('img');
+                    var url = window.URL || window.webkitURL;
+                    $('#ur-pic').css('background-image','url('+url.createObjectURL(data)+')');
+                    $('#pic-download').fadeIn().css("display","inline-block");
+                    $('.frais').fadeIn();
+                },
+                error:function(){
+                    
+                }
+            });
+    }
+
+        $("form.ajax-and-picture").submit(function(e) {
+                e.preventDefault();
+                var myForm = $(this)[0];
+                var url = $(this).attr('action');
+                jQuery.ajax({
+                    type: "POST",
+                    url:url,
+                    cache:false,
+                    processData: false,
+                    contentType: false,
+                    headers: { 'x-api-key': '147d07b083abc001c55bbc9ba5c3138c79711358' },
+                    data: new FormData(myForm),
+                    xhr:function(){
+                        var xhr = new XMLHttpRequest();
+                        xhr.responseType= 'blob'
+                        return xhr;
+                    },
+                    success: function(data){
+                        var img = document.getElementById('img');
+                        var url = window.URL || window.webkitURL;
+                        $('#ur-pic').css('background-image','url('+url.createObjectURL(data)+')')
+                    },
+                    error:function(){
+                        
+                    }
+                });
+
+    });
+
+    $('#pic-download').on('click', function() {
+        html2canvas($(".background-kc")[0]).then((canvas) => {
+            var a = document.createElement('a');
+                a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg");
+                a.download = 'PP-KC-Twitter.jpg';
+                a.click();
+        });
+
+    });
+
+
+});
+
+
+
+</script>
+@endsection
